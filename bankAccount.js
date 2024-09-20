335634a37c24c4d1f011a565d8c5107bc08b43a7
codebiscuits <ross_coates82@hotmail.com>
2024-09-20 14:11:41 +0100

    made changes to the css to make the buttons more legible and changed the messages in the class methods to give more information

--- bankAccount.js
+++ bankAccount.js
@@ -8,18 +8,18 @@ export class bankAccount {
     }
 
     checkBalance() {
-        console.log("Current balance: ", this.balance);
+        console.log("Current balance for", this.firstName, this.lastName, ":", this.balance);
     }
 
     deposit(amount) {
         this.balance += Number(amount);
-        console.log("Deposit successful!");
+        console.log("Deposit of", amount, "successful!");
     }
 
     withdraw(amount) {
         if (amount < this.balance) {
             this.balance -= Number(amount);
-            console.log("Withdrawal successful!");
+            console.log("Withdrawal of", amount, "successful!");
         } else {
             console.log("Not enough balance to withdraw " + amount);
         }
